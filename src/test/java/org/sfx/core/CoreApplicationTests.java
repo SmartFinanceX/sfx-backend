381a22ede4dfdfbd2c7f27d5a2e0362999b2a04b
AntiO2 <1219935161@qq.com>
2023-05-01 21:54:44 +0800

    init 初始化项目

---
+++ src/test/java/org/sfx/core/CoreApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sfx.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("项目初始化成功");
+	}
+
+}
