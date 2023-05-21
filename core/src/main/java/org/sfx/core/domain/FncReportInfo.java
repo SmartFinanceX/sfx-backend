97bae308fbdfa956f495d0c9fb6e4f82f301eeb7
AntiO2 <1219935161@qq.com>
2023-05-21 19:21:17 +0800

    fix(search) tablefield

--- core/src/main/java/org/sfx/core/domain/FncReportInfo.java
+++ core/src/main/java/org/sfx/core/domain/FncReportInfo.java
@@ -7,19 +7,11 @@ import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import lombok.Data;
 
 
-//@Data
-//@TableName(value = "inc_finc_info", autoResultMap=true)
-//public class FncReportInfo {
-//    String ticker;
-//    Short category;
-//    @TableField(typeHandler = JacksonTypeHandler.class)
-//    JSONArray finanace_data;
-//}
 @Data
-@TableName(value = "inc")
+@TableName(value = "inc_finc_info", autoResultMap=true)
 public class FncReportInfo {
     String ticker;
     Short category;
-    @TableField(value = "data")
-    String data;
+    // @TableField(typeHandler = JacksonTypeHandler.class)
+    String finanace_data;
 }
