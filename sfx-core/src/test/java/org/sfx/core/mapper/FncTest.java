6fdaddb22e7cde798276e8ac02fe724beab8b158
AntiO2 <antio2@qq.com>
2023-12-25 22:08:26 +0800

    fix(core) 财务数据

--- sfx-core/src/test/java/org/sfx/core/mapper/FncTest.java
+++ sfx-core/src/test/java/org/sfx/core/mapper/FncTest.java
@@ -1,6 +1,7 @@
 package org.sfx.core.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.sfx.api.domain.FncReportInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class FncTest {
     @Autowired
     FncMapper fncMapper;
+
     @Test
     public void FncInfoTest() {
         LambdaQueryWrapper<FncReportInfo> fncReportInfoLambdaQueryWrapper = new LambdaQueryWrapper<>();
-        fncReportInfoLambdaQueryWrapper.eq(FncReportInfo::getTicker,"000001").eq(FncReportInfo::getCategory,1);
+        fncReportInfoLambdaQueryWrapper.eq(FncReportInfo::getTicker, "000001").eq(FncReportInfo::getCategory, 1);
         System.out.println(fncMapper.selectList(fncReportInfoLambdaQueryWrapper));
     }
 
+    @Test
+    public void FncInfoTest2() {
+        QueryWrapper<FncReportInfo> fncReportInfoLambdaQueryWrapper = new QueryWrapper<>();
+        fncReportInfoLambdaQueryWrapper.eq("ticker", "1").eq("category", 1);
+        System.out.println(fncMapper.selectList(fncReportInfoLambdaQueryWrapper));
+    }
+
+    @Test
+    public void FncInfoTest3() {
+    }
 }
--- sfx-core/src/test/java/org/sfx/core/mapper/IncTest.java
+++ sfx-core/src/test/java/org/sfx/core/mapper/IncTest.java
@@ -1,13 +1,14 @@
 package org.sfx.core.mapper;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.sfx.core.domain.IncBasicInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class
-
 
 
 IncTest {
@@ -16,8 +17,14 @@ IncTest {
 
     @Test
     public void GetAllInfo() {
-        System.out.println(JSON.toJSONString( incBasicMapper.selectList(null)));
+        System.out.println(JSON.toJSONString(incBasicMapper.selectList(null)));
     }
 
+    @Test
+    public void GetOneInc() {
+        LambdaQueryWrapper<IncBasicInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(IncBasicInfo::getTicker, "000001");
+        System.out.println(JSON.toJSONString(incBasicMapper.selectList(wrapper)));
+    }
 
 }
